1a6d240c9f56a338f0d57773fca0700be0f10264
Rafael Cruzeiro <rafael.cruzeiro@me.com>
2023-05-23 19:05:57 -0300

    update gitignore file

--- Digitas.MarketData/Client/Requests/RequestData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Digitas.MarketData.Requests;
-
-public sealed record RequestData
-{
-    [JsonPropertyName("channel")]
-    public string? Channel { get; init; }
-}
